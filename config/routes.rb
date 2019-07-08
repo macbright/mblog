beec165a1d756eb20c86e134786535a7c64e83a0
bright okike <brightokike@gmail.com>
2019-07-08 21:12:55 +0300

    controllers and models generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,44 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'about/index'
+  namespace :admin do
+    get 'sessions/new'
+    get 'sessions/create'
+  end
+  namespace :admin do
+    get 'users/new'
+    get 'users/create'
+    get 'users/edit'
+    get 'users/update'
+    get 'users/destroy'
+    get 'users/index'
+    get 'users/show'
+  end
+  namespace :admin do
+    get 'comments/destroy'
+  end
+  get 'comments/new'
+  namespace :admin do
+    get 'categories/new'
+    get 'categories/create'
+    get 'categories/edit'
+    get 'categories/update'
+    get 'categories/destroy'
+    get 'categories/index'
+    get 'categories/show'
+  end
+  get 'categories/show'
+  namespace :admin do
+    get 'posts/new'
+    get 'posts/create'
+    get 'posts/edit'
+    get 'posts/update'
+    get 'posts/destroy'
+    get 'posts/index'
+    get 'posts/show'
+  end
+  get 'posts/index'
+  get 'posts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/admin/categories_controller_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get admin_categories_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get admin_categories_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get admin_categories_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get admin_categories_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get admin_categories_destroy_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get admin_categories_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get admin_categories_show_url
+    assert_response :success
+  end
+
+end
