908e0ef5715c90bd68cc4fce2f866e3e9922a37d
bright okike <brightokike@gmail.com>
2019-07-09 18:19:20 +0300

    admin setup

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,51 @@
 class Admin::UsersController < Admin::ApplicationController
   def new
+    @page_title = 'Add User'
+    @user = User.new
   end
 
   def create
+     @user = User.new(user_params)
+     if @user.save
+      flash[:notice] = 'User created'
+      redirect_to admin_users_path 
+     else 
+      render 'new'
+     end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = 'User Updated'
+
+      redirect_to admin_users_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    flash[:notice] = 'User Removed'
+
+    redirect_to admin_users_path
   end
 
   def index
+    @users = User.all 
   end
 
-  def show
-  end
+ 
+
+   private 
+    def  user_params
+      params.require(:user).permit(:name, :email, :password)
+    end 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,10 @@
 class Post < ApplicationRecord
     belongs_to :category
-    belongs_to :users
+    belongs_to :user
     has_many :comments
 
     validates :title, presence: true
     validates :category_id, presence: true 
     validates :body, presence: true 
-    
-
+   
 end
